ca7da5e42e529680892e1077b8482f0d6d296e4e
Aliciar-Thornthwaite <aliciathornthwaite@gmail.com>
2018-07-22 14:34:24 +0100

    Index.html Media Queries
    Flex box and Media queries used to update and make page more appealing visually. had to use Firefox to manage media queries

--- register.js
+++ register.js
@@ -2,13 +2,13 @@
 As seen on: https://codeburst.io/an-introduction-to-service-workers-in-javascript-27d6376460c2 */
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
+  navigator.serviceWorker.register('/sw.js')
   .then(function(response) {
 
     // Service worker registration done
-    console.log('Registration Successful', response);
+    console.log('Service Worker registration Successful', response);
   }, function(error) {
     // Service worker registration failed
-    console.log('Registration Failed', error);
+    console.log('Service Worker registration Failed', error);
   })
 };
--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
-/* Network Handling
-As understood from: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-and
-https://blog.sessionstack.com/how-javascript-works-service-workers-their-life-cycle-and-use-cases-52b19ad98b58 */
-var App_cache = 'application-cache';
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(App_cache).then(function(cache) {
-      console.log('cache opened');
+    caches.open('my-site-cache-v1').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
@@ -28,38 +21,32 @@ self.addEventListener('install', function(event) {
         '/img/8.jpg',
         '/img/9.jpg',
         '/img/10.jpg',
-      ]);
+      ]).catch(error => {console.log(error);
+      })
     })
   );
 });
 
 self.addEventListener('activate', function(event) {
-  var cacheName = ['pg-1, pg-2'];
+  var cacheWhitelist = ['my-site-cache-v2'];
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if (cacheName.indexOf(key) === -1) {
+        if (cacheWhitelist.indexOf(key) === -1) {
           return caches.delete(key);
-        }
-      }));
+          }
+        })
+      );
     })
   );
 });
-
-/* Network Handling
-As seen at: https://codeburst.io/an-introduction-to-service-workers-in-javascript-27d6376460c2 */
+/* As seen at: https://codeburst.io/an-introduction-to-service-workers-in-javascript-27d6376460c2 */
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // if Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+        })
+      );
+    });
