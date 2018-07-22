02eaed163615d16799e50c116bf207399448bbd6
Aliciar-Thornthwaite <aliciathornthwaite@gmail.com>
2018-07-22 23:30:01 +0100

    Service Worker Added
    Additions made to service worker which now works as intended

--- register.js
+++ register.js
@@ -2,13 +2,12 @@
 As seen on: https://codeburst.io/an-introduction-to-service-workers-in-javascript-27d6376460c2 */
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then(function(response) {
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
 
     // Service worker registration done
-    console.log('Service Worker registration Successful', response);
-  }, function(error) {
+    console.log('Service Worker registration Successful' + reg.scope);
+  }).catch (function(err){
     // Service worker registration failed
-    console.log('Service Worker registration Failed', error);
+    console.log('Service Worker registration Failed' + err);
   })
 };
--- sw.js
+++ sw.js
@@ -1,52 +1,65 @@
+var staticCache = 'restaurant-cache-v1';
+var urlsToCache = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/register.js',
+    '/js/restaurant_info.js',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
+
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('my-site-cache-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/restaurant.html',
-        '/css/styles.css',
-        '/data/restaurants.json',
-        '/js/dbhelper.js',
-        '/js/main.js',
-        '/register.js',
-        '/js/restaurant_info.js',
-        '/img/1.jpg',
-        '/img/2.jpg',
-        '/img/3.jpg',
-        '/img/4.jpg',
-        '/img/5.jpg',
-        '/img/6.jpg',
-        '/img/7.jpg',
-        '/img/8.jpg',
-        '/img/9.jpg',
-        '/img/10.jpg',
-      ]).catch(error => {console.log(error);
-      })
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['my-site-cache-v2'];
-
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(key);
-          }
+    // Perform install steps
+    event.waitUntil(
+        //open cache by name
+        caches.open(staticCache).then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
         })
-      );
-    })
-  );
+    );
 });
-/* As seen at: https://codeburst.io/an-introduction-to-service-workers-in-javascript-27d6376460c2 */
+
+// As interpreted from: https://blog.sessionstack.com/how-javascript-works-service-workers-their-life-cycle-and-use-cases-52b19ad98b58
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            // Cache hit - return response
+            return response || fetch(event.request);
         })
-      );
-    });
+    );
+});
+
+/* As understood at: https://codeburst.io/an-introduction-to-service-workers-in-javascript-27d6376460c2
+and https://css-tricks.com/serviceworker-for-offline/ */
+
+self.addEventListener('activate', function(event) {
+
+    event.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(
+                keys.filter(function(key) {
+                    return !key.startsWith('restaurant-');
+                }))
+            map(function(key) {
+                return caches.delete(key);
+            })
+        })
+        .then(function() {
+            console.log('activation complete');
+        })
+    );
+});
